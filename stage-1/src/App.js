778ab9775d6992a74661abd820f9217abb4b5e71
thomas brown <thomasbrown9101@gmail.com>
2019-07-02 14:09:20 -0700

    finished stage one

--- stage-1/src/App.js
+++ stage-1/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import StoreFront from "./Components/StoreFront/StoreFront";
-import ShoppingCart from "./Components/ShoppingCart/ShoppingCart";
-import NavBar from "./Components/NavBar/NavBar";
+import ShoppingCart from "./Components/ShoppingCart/ShoppingCart.js";
+import NavBar from "./Components/NavBar/NavBar.js";
+import axios from 'axios';
 
 class App extends Component {
   constructor(props) {
@@ -16,8 +17,7 @@ class App extends Component {
     this.navigate = this.navigate.bind(this);
   }
   componentDidMount() {
-    axios
-      .get("https://practiceapi.devmountain.com/products/")
+    axios.get("https://practiceapi.devmountain.com/products/")
       .then(response => {
         this.setState({
           products: response.data
@@ -48,15 +48,15 @@ class App extends Component {
     }
   }
   render() {
-    const { products, showCart } = this.state;
+    const { products, showCart, cart } = this.state;
     return (
       <div className="App">
         <NavBar navigate={this.navigate} />
         <div className="main-container">
           {showCart ? (
-            <ShoppingCart cart={cart} />
+            <ShoppingCart cart={cart} removeFromCart={this.removeFromCart} />
           ) : (
-            <StoreFront products={products} />
+            <StoreFront products={products} addToCart={this.addToCart} />
           )}
         </div>
       </div>
